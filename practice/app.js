b1c2c623ad82eea35ed69079b5a0e8b2e7bc66b1
maximum259492 <enimum259492@gmail.com>
2024-03-07 11:39:54 +0200

    Boolean, Преобразование Типов, Шаблонные строки

--- practice/app.js
+++ practice/app.js
@@ -1,23 +1,45 @@
-/*
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40
-часов работы.
-Сейчас понедельник.
-Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
+const projectName = "Сайт магазина";
+const price = 2000;
+const author = "Василий Пупкин";
 
-const payRateUSD = 80;
-const projectHours = 40;
-const daysIhave = 9;
-const daysIneed = 7;
+const template = author + " заказал " + projectName + " по цене " + price + "грн";
+console.log(template);
 
-console.log("Успею ли я сделать заказ: " + (daysIneed < daysIhave))
 
-const moneyIrequest = projectHours * payRateUSD
-console.log("За 40 часов работы я запрошу:", moneyIrequest + "$",)
+const template2 = `${author} заказал ${projectName} по цене ${price}грн `
+console.log(template2);
 
+const template3 = 'Проект \n' + 'цена:' + price + '$';
+console.log(template3);
+
+const template4 = `Проект;
+цена: ${price}$`;
+console.log(template4);
+
+
+
+// Преобразование типов
+
+const age = '18';
+console.log(Number(age) + 5);
+console.log(age - 3);
+const userName = 'vasya';
+console.log(Number(userName) * 2);
+console.log(typeof NaN);
+console.log(String(4) + 7);
+console.log(Boolean('') + 10);
+console.log(true + 2);
+
+const a = 2 + '10'; // 210
+console.log(a - 10) // 200
+
+
+// Boolean
+
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+console.log(Boolean(Number('sx')))
+console.log(Boolean(NaN))
 
