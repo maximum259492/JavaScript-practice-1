fc00c17b459f9d97f86655473c1378e9044626ca
maximum259492 <enimum259492@gmail.com>
2024-03-09 11:41:53 +0200

    Задача на функцию в функции

--- practice/app.js
+++ practice/app.js
@@ -1,62 +1,70 @@
+// ПАРАМЕТРЫ ПО УМОЛЧАНИЮ ПАРАМЕТРЫ ПО УМОЛЧАНИЮ ПАРАМЕТРЫ ПО УМОЛЧАНИЮ
 
-// ФУНКЦИИ ФУНКЦИИ ФУНКЦИИ ФУНКЦИИ ФУНКЦИИ ФУНКЦИИ ФУНКЦИИ 
-// function logName(name, surname) {
-//     console.log(`Мое имя ${name} ${surname}`)
+
+// function toPower(num = 4, power = 2) {
+//     const res = num ** power;
+//     return res;
 // }
 
-// logName('Максон', 'Билык')
+// console.log(toPower(2, 3))
+// console.log(toPower(2))
+// console.log(toPower())
 
-// function countDepositSum(depositInUSD, month, rate) {
-//     return depositInUSD * (1 + rate / 12) ** month
+// Условия в функциях Условия в функциях Условия в функциях Условия в функциях Условия в функциях
+
+// function canAccessWebsite(age) {
+//     if (age < 18) {
+//         return `Неа`;    // если мы попадаем на RETURN - выполнение функции завершается
+//     }
+//     return `Да, заходи, пожалуйста`;
 // }
 
-// const example1 = countDepositSum(1000, 36, 0.1)
-// console.log(example1)
+// console.log(canAccessWebsite(17))
 
-// const example2 = countDepositSum(1000, 48, 0.1)
-// console.log(example2)
+// const canAccessWebsite2 = age => age < 18 ? `Неа` : `Ага`
+// console.log(canAccessWebsite2(17))
 
-// АНОНИМНЫЕ ФУНКЦИИ АНОНИМНЫЕ ФУНКЦИИ АНОНИМНЫЕ ФУНКЦИИ 
+// Функции в Функциях Функции в Функциях Функции в Функциях Функции в Функциях Функции в Функциях
 
-// function powerOfTwo(num) {
-//     return num * num;
+// const KG_IN_USD = 7;
+// const KM_IN_USD = 5;
+
+// const calculateW = present => present * KG_IN_USD
+// const calculateD = distance => distance * KM_IN_USD
+
+// function getExchangePrice(present1, present2, distance) {
+//     const price1 = calculateW(present1)
+//     const price2 = calculateW(present2)
+//     const distancePrice = calculateD(distance)
+//     return price1 + price2 + distancePrice
 // }
-// console.log(powerOfTwo(11))
+// console.log(getExchangePrice(1, 2, 10))
+
+/* Пользователь: 
+    - Возраст 
+    - Наличие работы
+    - Деньги
+    Нужно проверить может ли он купить новый MacBook за 2000$? 
+    Он может брать не только свои деньги, но и взять кредит.
+    Ему дадут 500$, только если ему больше 24-х лет и он имеет работу, 100$ если ему просто больше 24-х лет и 0 в ином случае.
+    Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false; */
 
 
-// const pOft = function (num) { // Анонимная - нет названия, содержиться в переменной 
-//     return num * num;
-// }
-
-// console.log(pOft(12))
-
-// СТРЕЛОЧНЫЕ ФУНКЦИИ  СТРЕЛОЧНЫЕ ФУНКЦИИ  СТРЕЛОЧНЫЕ ФУНКЦИИ $
-
-// function powerOfTwo(num) {
-//     return num * num
-// }
-
-// console.log(powerOfTwo(5))
-
-// const poft = (num) => num * num // Стрелочная функция 
-// const pofg = num => num * num // Можно опустить скобки аргумента 
-// const arrowExample = (num, i) => { // Если 2 аргумента -> ставим Скобки 
-//     console.log(num);
-//     return num * num
-// }
-// console.log(poft(4))
-// console.log(pofg(6))
-// console.log(arrowExample(8))
-
-/* Переписать функцию в стрелочную */
-
-function toPower(num, power) {
-    const res = num ** power
-    return res
+const isAbleToTakeCredit = (age, isEmployed = false) => {
+    if (age >= 24 && isEmployed === true) {
+        return 500;
+    } 
+    else if (age >= 24) {
+        return 100;
+    }
+        return 0
 }
 
-console.log(toPower(2, 3))
+function isAbleToBuy(age, cash, productPrice, isEmployed = false) {
+    const credit = isAbleToTakeCredit(age, isEmployed)
+    return (cash + credit) >= productPrice ? `Да он в крипте работает` : `Чел, ты... ну не нужен тебе MAC`
 
-const toPowerArrow = (num,power) => num ** power
+}
 
-console.log(toPowerArrow(5,3))
+console.log(isAbleToBuy(25, 1900, 2000, false))
+
