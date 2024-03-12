013bf118dc0124fd47456f659178df3af95c63ae
maximum259492 <enimum259492@gmail.com>
2024-03-12 16:23:40 +0200

    Цикл в цикле, цикл while. For ... of / For ... in. Задачка

--- practice/app.js
+++ practice/app.js
@@ -1,46 +1,135 @@
-// Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR Цикл FOR 
+// Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле
 
+// for (let i = 1; i < 5; i++) {
+//     console.log(`Цикл 1 - ${i}`);
+//     for (let j = 1; j < 5; j++) {
+//         console.log(`Цикл 2 - ${j}`)
+//     }
 
-
-// for (let i = 1; i < 10; i++) {
-
-//     console.log(`Наш баланс ${i}$`)
 // }
 
-
-// BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE BREAK и CONTINUE 
-
-// const tasks = ['Задача 1', 'Задача 2', 'Задача 3']
+// const tasks = [[1, 'Задача 1'], [2, 'Задача 2']]
 
 // for (let i = 0; i < tasks.length; i++) {
-//     if (tasks[i] === 'Задача 1') {
-//         continue; // Continue прерывает выполнение текущей итерации текущего или отмеченного цикла, и продолжает его выполнение на следующей итерации. => 'Задача 2' skipped
+//     for (let j = 0; j < tasks[i].length; j++) {
+//         console.log(tasks[i][j]) // Берет i-тый элемент массива, который является массивом и достает оттуда элемент j
 //     }
-//     console.log(tasks[i])
+
 // }
 
-// console.log('----')
 
-// for (let i = 0; i < tasks.length; i++) {
-//     if (tasks[i] === 'Задача 2') {
-//         break; // Break прерывает выполнение всего цикла 
-//     }
-//     console.log(tasks[i])
+// ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE
+
+// for (let i = 1; i < 5; i++) {
+//     console.log(`Вывод - ${i}`)
 // }
+
+// let i = 1;
+// while (i < 5) {
+//     console.log(`Вывод - ${i}`)
+//     i++;
+// }
+
+
+// const arr = [1, 4, 8, 7]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 5) {
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+
+// let i = 0
+// while (arr[i] <= 5 && i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+
+// let j = 0
+// do{
+//     console.log(j)
+//     j++
+// } while(j < 0) // do while - Выполняется хотя бы один раз
+
+
+// ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN 
+
+// const arr = [1, 4, 8, 7]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// const arr = [1, 4, 8, 7, 'Макс']
+
+// for (let element of arr) { // Получение каждого элемента из массива arr
+//     console.log(element)
+// }
+
+// for (let index in arr) { // Получение идекса каждого элемента массива arr
+//     console.log(index)
+// }
+
 
 /*
-    Задача вывести в консоль строку "Я люблю JS !" из массива,
-    проходя циклом в обратном порядке, не используя метод reverse.
-    const arr = ['!', 'JS', '  люблю', 'Я' ]
+    Есть выгрузка операций пользователя
+    const operations = [1000, -700, 300, -500, 10000]
+    а так же начальный баланс в 100$
+    Необходимо сделать функции расчета:
+    - Итогового баланса 
+    - Наличия отрицательного балланса (если после очередной операции баланс < 0, то выдавать false)
+    - Расчета среднего расхода и среднего дохода
  */
-const arr = ['!', 'JS', 'люблю', 'Я'];
-const sentence = []
-for (let i = arr.length - 1; i >= 0; i--) {
+
+const operations = [1000, -700, 300, -500, 10000]
+const Expenses = [];
+const income = [];
+const startingBalance = 100;
 
 
-    let word = arr[i]
-    sentence.push(word)
-    
-
+function finalBalance(arrayOfOperations, initialBalance) {
+    let balance = initialBalance
+    for (const element of arrayOfOperations) {
+        balance += element
+    }
+    return balance
 }
-console.log(sentence.join(' '))
+
+console.log(finalBalance(operations, startingBalance))
+
+function isOkay(arrayOfOperations, initialBalance) {
+    let balance = initialBalance
+    let isOkay = true
+    for (const element of arrayOfOperations) {
+        balance += element
+        if (balance < 0) {
+            isOkay = false
+            break;
+        }
+    }
+    return isOkay
+}
+
+console.log(isOkay(operations, startingBalance))
+
+
+function average(arrayOfOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positiveCount++;
+            positiveSum += element;
+        }
+        if (element < 0) {
+            negativeCount++;
+            negativeSum += element
+        }
+    }
+    return [positiveSum / positiveCount, negativeSum / negativeCount]
+}
+
+console.log(average(operations))
