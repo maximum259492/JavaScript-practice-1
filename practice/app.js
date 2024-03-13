d8a6f036d73767a733966b2ef05474a98e80efd5
maximum259492 <enimum259492@gmail.com>
2024-03-13 17:09:57 +0200

    Быстрое создание Массива

--- practice/app.js
+++ practice/app.js
@@ -1,41 +1,14 @@
-// Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap Метод Flat/FlatMap 
+const arr = [1, 2, 3, 4, 5];
 
-const prices = [[2, 4], [3, 4], [10, 20]]
+console.log(new Array(1, 2, 3, 4, 5))
+const arr2 = new Array(5) // Создаем пустой массив с размером 5 
+console.log(arr2)
+console.log(arr2.map(el => 1))
+
+arr2.fill(1, 0, 3) // Заполняем массив единицей с позиции 0 до 3
+arr2.fill(2, 3, 5) // Заполняем массив двойкой с позиции 3 до 5
+console.log(arr2)
 
 
-
-// Метод flat() возвращает новый массив, в котором все элементы вложенных подмассивов были рекурсивно "подняты" на указанный уровень depth.
-
-const res = prices.flat();
-
-// Метод flatMap() позволяет сформировать массив, применяя функцию к каждому элементу, затем уменьшает вложенность, делая этот массив плоским, и возвращает его. 
-const res2 = prices.flatMap((el, index) => el[1]);
-console.log(res)
-console.log(res2)
-
-// Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort Метод Sort 
-
-const users = ['Vasya', 'Masha', 'Katya', 'Anya']
-console.log(users)
-
-// Метод sort() на месте сортирует элементы массива и возвращает отсортированный массив.
-users.sort();
-console.log(users)
-
-const operations = [100, -300, 0, -100, 50, 480]
-console.log(operations)
-
-// return < 0 - a, b - сохраняем порядок
-// return  > 0 - b, a - меняем порядок      
-operations.sort((a, b) => {
-    if (a > b) {
-        return -1
-    }
-    if (a < b) {
-        return 1
-    }
-});
-console.log(operations)
-
-operations.sort((a, b) => a - b);
-console.log(operations)
+const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1) // создает массив с элементами от 1 до 5
+console.log(arr3)
