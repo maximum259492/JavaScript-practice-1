f1552f395808f3067d11148facba17b40046a26b
maximum259492 <enimum259492@gmail.com>
2024-03-13 09:33:12 +0200

    Функции высшего Порядка, CallBack, Возврат функции

--- practice/app.js
+++ practice/app.js
@@ -1,135 +1,54 @@
-// Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле Цикл в Цикле
-
-// for (let i = 1; i < 5; i++) {
-//     console.log(`Цикл 1 - ${i}`);
-//     for (let j = 1; j < 5; j++) {
-//         console.log(`Цикл 2 - ${j}`)
-//     }
-
+// function add(a, b) {
+//     return a + b
 // }
 
-// const tasks = [[1, 'Задача 1'], [2, 'Задача 2']]
+// function subtract(a, b) {
+//     return a - b
+// }
 
-// for (let i = 0; i < tasks.length; i++) {
-//     for (let j = 0; j < tasks[i].length; j++) {
-//         console.log(tasks[i][j]) // Берет i-тый элемент массива, который является массивом и достает оттуда элемент j
-//     }
-
+// function power(a, b) {
+//     return a ** b
 // }
 
 
-// ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE ЦИКЛ WHILE
+// Функция высшего порядка
 
-// for (let i = 1; i < 5; i++) {
-//     console.log(`Вывод - ${i}`)
+// function calculate(a, b, fn) {
+//     console.log(fn.name);
+//     const res = fn(a, b);
+//     return res
 // }
 
-// let i = 1;
-// while (i < 5) {
-//     console.log(`Вывод - ${i}`)
-//     i++;
-// }
+// let res = calculate(3, 5, add)
+// console.log(res)
+// res = calculate(3, 5, subtract)
+// console.log(res)
+// res = calculate(3, 5, power)
+// console.log(res)
 
 
-// const arr = [1, 4, 8, 7]
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > 5) {
-//         break;
-//     }
-//     console.log(arr[i])
-// }
+// Возврат функции Возврат функции Возврат функции Возврат функции Возврат функции Возврат функции Возврат функции Возврат функции Возврат функции 
 
-// let i = 0
-// while (arr[i] <= 5 && i < arr.length) {
-//     console.log(arr[i]);
-//     i++;
-// }
-
-
-// let j = 0
-// do{
-//     console.log(j)
-//     j++
-// } while(j < 0) // do while - Выполняется хотя бы один раз
-
-
-// ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN ЦИКЛЫ FOR OF и FOR IN 
-
-// const arr = [1, 4, 8, 7]
-
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-// }
-
-// const arr = [1, 4, 8, 7, 'Макс']
-
-// for (let element of arr) { // Получение каждого элемента из массива arr
-//     console.log(element)
-// }
-
-// for (let index in arr) { // Получение идекса каждого элемента массива arr
-//     console.log(index)
-// }
-
-
-/*
-    Есть выгрузка операций пользователя
-    const operations = [1000, -700, 300, -500, 10000]
-    а так же начальный баланс в 100$
-    Необходимо сделать функции расчета:
-    - Итогового баланса 
-    - Наличия отрицательного балланса (если после очередной операции баланс < 0, то выдавать false)
-    - Расчета среднего расхода и среднего дохода
- */
-
-const operations = [1000, -700, 300, -500, 10000]
-const Expenses = [];
-const income = [];
-const startingBalance = 100;
-
-
-function finalBalance(arrayOfOperations, initialBalance) {
-    let balance = initialBalance
-    for (const element of arrayOfOperations) {
-        balance += element
+function power(pow) {
+    return function (num) {
+        return num ** pow
     }
-    return balance
 }
 
-console.log(finalBalance(operations, startingBalance))
 
-function isOkay(arrayOfOperations, initialBalance) {
-    let balance = initialBalance
-    let isOkay = true
-    for (const element of arrayOfOperations) {
-        balance += element
-        if (balance < 0) {
-            isOkay = false
-            break;
-        }
-    }
-    return isOkay
-}
+const power = pow => num => num ** pow // Стрелочная функция callBack
 
-console.log(isOkay(operations, startingBalance))
+const powerOfTwo = power(2); // Нашу функцию положили в переменную 
+console.log(powerOfTwo(5))
+console.log(powerOfTwo(10))
+
+const powerOfThree = power(3)
+console.log(powerOfThree(5))
+
+console.log(power(5)(4))
 
 
-function average(arrayOfOperations) {
-    let positiveCount = 0;
-    let positiveSum = 0;
-    let negativeCount = 0;
-    let negativeSum = 0;
-    for (const element of arrayOfOperations) {
-        if (element > 0) {
-            positiveCount++;
-            positiveSum += element;
-        }
-        if (element < 0) {
-            negativeCount++;
-            negativeSum += element
-        }
-    }
-    return [positiveSum / positiveCount, negativeSum / negativeCount]
-}
 
-console.log(average(operations))
+
+
+
