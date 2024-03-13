243e8accf5a4d42c7c16da61ea9d4066a4e13b6a
maximum259492 <enimum259492@gmail.com>
2024-03-13 18:06:34 +0200

    Метод Reduce,  Методы Find, FindIndex и Метод Some

--- practice/app.js
+++ practice/app.js
@@ -1,14 +1,99 @@
-const arr = [1, 2, 3, 4, 5];
+// Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE Метод REDUCE
 
-console.log(new Array(1, 2, 3, 4, 5))
-const arr2 = new Array(5) // Создаем пустой массив с размером 5 
-console.log(arr2)
-console.log(arr2.map(el => 1))
-
-arr2.fill(1, 0, 3) // Заполняем массив единицей с позиции 0 до 3
-arr2.fill(2, 3, 5) // Заполняем массив двойкой с позиции 3 до 5
-console.log(arr2)
+// const operations = [100, -20, 7, -30, 50]
+// let balance = 0
+// for (const operation of operations) {
+//     balance += operation
+// }
+// console.log(balance);
 
 
-const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1) // создает массив с элементами от 1 до 5
-console.log(arr3)
+
+// Метод Reduce() применяет функцию к аккумулятору и каждому элементу массива (слева направо), чтобы свести массив к одному значению.
+
+// const finalBalance = operations.reduce((acc, operation, i) => {
+//     console.log(`Итерация ${i + 1} acc: ${acc}, operation ${operation}`)
+//     return acc += operation
+// }, 0)
+
+// 0 - acc = 0, value = 100
+// 1 - acc = 100, value = -20
+
+// console.log(finalBalance);
+
+// const minElement = operations.reduce((acc, operation, i) => {
+//     if (operation > 0) {
+//         return acc;
+//     } else {
+//         return operation
+//     }
+
+// }, 0)
+// console.log(minElement)
+
+/*
+    Найти среднее значение последовательности
+    чисел с помощью reduce()
+ */
+
+// const arr = [2, 4, 4, 10]
+
+// const average = arr.reduce((acc, num, i) => {
+//     acc += num
+//     if (i === arr.length - 1) {
+//         return acc / arr.length
+//     }
+//     else {
+//         return acc
+//     }
+
+// }, 0)
+// console.log(average)
+
+
+
+// Метод Find и FindIndex
+
+// const arr = [2, 4, 4, 10, 20]
+// let elGT5;
+
+// for (const el of arr) {
+//     if (el > 5) {
+//         elGT5 = el
+//         break;
+
+//     }
+// }
+
+// console.log(elGT5)
+
+// elGT5 = arr.find(el => el > 5)
+// elGT5Index = arr.findIndex(el => el < 0)
+// console.log(elGT5)
+// console.log(elGT5Index)
+
+
+/* Написать функцию, которая возвращает true,
+    если элемент есть, и false, если нет
+*/
+
+const arr = [2, 4, 4, 10, 20]
+
+// function some(arr, el){
+//     let check = arr.includes(el)
+//     console.log(check)
+// }
+
+// some(arr,4)
+
+
+function some(array, element) {
+    const res = array.find(el => {
+        return el === element
+    })
+    return res === undefined ? false : true
+}
+
+console.log(some(arr, 10))
+
+console.log(arr.some(el => el === 10)) // Метод Some() проверяет, если ли в масстве элемент равный задаваемому
