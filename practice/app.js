b79a77fda67439296a7174cdf730e761d01e7c98
maximum259492 <enimum259492@gmail.com>
2024-03-17 11:22:04 +0200

    Обьекты, методы обьектов, Задачка Кошелек на методы Обьектов

--- practice/app.js
+++ practice/app.js
@@ -1,26 +1,137 @@
-// const userFullName = 'Vasya Pupkin Vasilevich'
-// console.log(userFullName.split(' ')) // Создает из строки Массив с определенным разделителем
-// const [firstName, surName, lastName] = userFullName.split(' ')
-// console.log(firstName)
-// console.log(surName)
-// console.log(lastName)
 
-// const arr = ['Ты', 'знаешь', 'JS']
-// console.log(arr.join(' ')) // Создает из массива Строку с определенным разделителем
+// const user = {
+//     name: 'Vasya',
+//     surname: 'Pupkin',
+//     age: 24,
+//     skills: [
+//         'Программирование',
+//         'Готовка'],
+//     eduBasic: 'Школа 10',
+//     eduPro: 'МФТИ'
+// }
+// console.log(user.city)
+// console.log(user.skills);
+// const level = 'Pro'
+// console.log(user['edu' + level])
+// // Если мы хотим к нерасчетному свойству используем .
+// // Если свойство расчетное(получаем извне),
 
 
-// const film = 'Звёздные войны'
-// console.log(film.padStart(50, '*')) // добавляет в начало к строке 50 - film.length пробелы, или указанный символ 
-// console.log(film.padEnd(50, '*')) // обавляет в конец строки 50 - film.length пробелы, или указанный символ 
-// console.log(film.repeat(50, ' ')) // Повторяет указанное колво раз строку, и делит ее указаным разделителем
+// // const res = prompt('Введие свойство')
 
-/* Замаскировать все, кроме последник 4х символов */
-const card = '2342834503458353'
+// user['city'] = 'Kiev'
+// user.city = 'Kive'
+// console.log(user.city)
 
-console.log(card)
-console.log(card.length)
+// user.age = 30;
+// user['age'] = 30
+// console.log(user)
 
-console.log(card.slice(-4).padStart(16, '*'))
+/* отсортировать пользователей по возрасту */
+// const users = [
+//     { name: 'Vasya', age: 30 },
+//     { name: 'Katya', age: 18 },
+//     { name: 'Anya', age: 40 },
+//     { name: 'Petya', age: 25 }
+// ]
+
+
+// const sorted = users.sort(((a, b) => {
+//     return a.age - b.age
+// }))
+
+// console.log(sorted)
+
+/* Преобразовать пользователей до вида
+   {fullName: 'Вася Пупкин', skillnum: 2}
+*/
+
+// const users = [
+//     {
+//         name: 'Вася',
+//         surname: 'Пупкин',
+//         age: 30,
+//         skills: ['Разработка', 'DevOps']
+//     },
+//     {
+//         name: 'Катя',
+//         surname: 'Белова',
+//         age: 18,
+//         skills: ['Дизайн']
+
+//     }
+
+// ]
+
+// const newUsers = users.map(user => {
+//     return {
+//         fullName: `${user.name} ${user.surname}`,
+//         skillnum: user.skills.length
+//     }
+// })
+// console.log(newUsers)
+
+// Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов Методы Обьектов 
+
+// const user = {
+//     name: 'Vasya',
+//     surname: 'Pupkin',
+//     age: 24,
+//     getFullName: function () {
+//         console.log(this)
+//         return this.name + ' ' + this.surname
+//     }
+// }
+// console.log(user.getFullName())
+
+/*
+    Реализовать методы увеличения и уменьшения баланса,
+    при котором каждая операция сохраняется в массив 
+    operations в виде {reason: 'Оплата налогов', sum: -100}. Возвращается true, если успешно и false, если не хватает баланса.
+    Так же реализовать метод вывода числа операций по кошельку
+*/
+
+const wallet = {
+    balance: 600,
+    operations: [],
+    increasedBalance: function (reason, summ) {
+        this.balance += summ
+        this.operations.push({
+            reason: reason,
+            summ: +summ
+        })
+        console.log(`УСПЕХ! Текущий баланс: ${this.balance}`)
+        return true
+    },
+    decreasedBalance: function (reason, summ) {
+        if (this.balance <= summ) {
+            console.log(`Недостаточно денег на балансе: ${this.balance}`)
+            return false
+        } else {
+            this.balance -= summ
+            this.operations.push({
+                reason: reason,
+                summ: -summ
+            })
+            console.log(`УСПЕХ! Текущий баланс: ${this.balance}`)
+            return true
+        }
+
+    },
+    operationCounter: function () {
+        return `Число операций: ${this.operations.length}`
+    }
+
+}
+wallet.increasedBalance('Заработок', 500)
+
+wallet.decreasedBalance('Налог', 1000)
+
+
+console.log(wallet.operationCounter())
+console.log(wallet.operations)
+
+
 
 
 
